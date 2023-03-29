185916ef7dd7a15759813686091ed6d2cb7edd40
macservis <fboqiyev1065@gmail.com>
2023-03-29 17:17:00 +0500

    stage0-module4-loops-taskFinal updated

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,10 +2,14 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int a = 1;
+        for (int i = 0; i < power; i++){
+            a *= numberToPrint;
+        }
+        System.out.println(a);
     }
 
     public static void main(String[] args) {
-        new Power().printPower(10, 3);
+        new Power().printPower(-10, 3);
     }
 }
